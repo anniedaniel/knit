ba244ee3f4e53b536a3b7d4d60861b87147b4c66
anniedaniel <anniedaniel001@gmail.com>
2016-07-24 20:00:50 -0500

    everything loads and it's v ugly

--- js/main.js
+++ js/main.js
@@ -5,10 +5,24 @@ Tabletop.init( { key: '1w40A5LqRz3ZLPu3zIwE5tCUsNI81I8XgfsZ9J2DiG7k',
 });
 
 function showInfo(data, tabletop) {
-	var source = $(".template-video").html();
-	var template = Handlebars.compile(source);
+	var video = $(".template-video").html();
+	var videoTemplate = Handlebars.compile(video);
 	$.each( tabletop.sheets("videos").all(), function(i, video) {
-	  var html = template(video);
+	  var html = videoTemplate(video);
 	  $("#videos").append(html);
+	});
+
+	var pattern = $(".template-pattern").html();
+	var patternTemplate = Handlebars.compile(pattern);
+	$.each( tabletop.sheets("patterns").all(), function(i, pattern) {
+	  var html = patternTemplate(pattern);
+	  $("#patterns").append(html);
+	});
+
+	var resource = $(".template-resource").html();
+	var resourceTemplate = Handlebars.compile(resource);
+	$.each( tabletop.sheets("resources").all(), function(i, resource) {
+	  var html = resourceTemplate(resource);
+	  $("#resources").append(html);
 	});
 }
