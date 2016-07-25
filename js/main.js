3bf3c3acef5b711073b70f7e72f2ceaf58675d23
anniedaniel <anniedaniel001@gmail.com>
2016-07-24 22:41:40 -0500

    videos load

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $(document).ready( function() {
-Tabletop.init( { key: '1w40A5LqRz3ZLPu3zIwE5tCUsNI81I8XgfsZ9J2DiG7k',
+	Tabletop.init( { key: '1w40A5LqRz3ZLPu3zIwE5tCUsNI81I8XgfsZ9J2DiG7k',
                  callback: showInfo,
                  parseNumbers: true } );
+	$('.video-frame').fitVids();
 });
 
 function showInfo(data, tabletop) {
