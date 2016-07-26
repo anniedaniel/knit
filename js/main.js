c15255d97fa97f34302a4dee7dc9fe98752d71f3
anniedaniel <anniedaniel001@gmail.com>
2016-07-25 20:48:27 -0500

    responsive videos

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ $(document).ready( function() {
 	Tabletop.init( { key: '1w40A5LqRz3ZLPu3zIwE5tCUsNI81I8XgfsZ9J2DiG7k',
                  callback: showInfo,
                  parseNumbers: true } );
-	$('.video-frame').fitVids();
+	var video = $('.video-frame').fitVids();
+	console.log(video);
 });
 
 function showInfo(data, tabletop) {
